61e6e7a80b1fb2f3cc4f804c30c541a34c0caff5
Ryan Charette <ryan@ryancharette.com>
2020-08-09 02:21:33 -0500

    Tmux updates

--- .tmux/plugins/battery.sh
+++ .tmux/plugins/battery.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+os=$(uname -s)
 
-percent=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+if [[ $os == 'Darwin' ]]; then
+ percent=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+else
+  percent=$(acpi | awk '{ print $4 }' | sed 's/%,\?//')
+fi
 
 if (( $percent > 59  )); then
-  echo "#[fg=#4ecf6c] #[italics]$percent%#[none]"
+  echo "#[fg=#4ecf6c]#[italics]$percent%#[none]"
 elif (( $percent > 19 )); then
-  echo "#[fg=#bdb43c]ﯜ #[italics]$percent%#[none]"
+  echo "#[fg=#bdb43c]#[italics]$percent%#[none]"
 else
-  echo "#[fg=#cc0a00] #[italics]$percent%#[none]"
+  echo "#[fg=#cc0a00]#[italics]$percent%#[none]"
 fi
